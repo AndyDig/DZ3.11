fc50de7b5dd086385d650bf12d32eabd12e8b0b6
AndyDig <126315776+AndyDig@users.noreply.github.com>
2023-03-23 01:03:32 +0300

    DZ3.11

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 8098;
+        if (a > 0) {
+            System.out.println("Положительное");
+        } else {
+            System.out.println("Отрицательное");
+
+        }
+    }
+}
